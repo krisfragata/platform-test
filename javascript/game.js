543235333550b599e5f36a4aee90c154aef1dab0
kellogsricekrispy <79350471+kellogsricekrispy@users.noreply.github.com>
2021-06-04 16:24:44 -0400

    rendering canvas and player

--- javascript/game.js
+++ javascript/game.js
@@ -1,39 +1,28 @@
-$(document).ready(()=>{
-
-const Game = function(){
-    this.world = {
-        backgroundColor: "rgba(197, 147, 90, 0.25)",
-        
-        player: new Game.Player(),
-        height: 72,
-        width: 128,
-
-        update: ()=>{
-            
+    const canvas = document.querySelector('#canvas');
+    const Game = function(){
+        this.world = {
+            backgroundColor: "rgba(255, 203, 108, 0.25)",
+            // "rgba(197, 147, 90, 0.25)",
+            player: new Game.Player(),
+            width: innerWidth,
+            height: innerHeight,
         }
-        
+
     };
 
-    this.update = ()=>{ this.world.update(); };
-};
+    Game.prototype = {constructor: Game};
+    Game.Player = function(x,y){
+        this.color = "#A195D6";
+        this.x = 200;
+        this.y = 911;
+        this.xVel = 0;
+        this.yVel = 0;
+        this.jump =true;
+        this.height = 130;
+        this.width = 70;
+    }
 
-Game.prototype = { constructor: Game};
+    Game.Player.prototype = {
+        constructor: Game.Player,
+    }
 
-Game.Player = (x,y)=>{
-    this.x =  100;
-    this.y = 20;
-    this.color = "#A195D6";
-    this.height = 20;
-    this.width = 20;
-    this.jumping = true;
-    xVel = 0;
-    yVel = 0;
-}
-
-Game.Player.prototype = {
-    constructor: Game.Player,
-
-}
-    
-
-})
--- javascript/script.js
+++ javascript/script.js
@@ -10,21 +10,25 @@ window.addEventListener('load', function(e){
     start.addEventListener('click', ()=>{
         start.style.display = 'none';
 
+        // objects
+        let game = new Game();
+        let display = new Display(document.querySelector('#canvas'))
+        const canvas = document.querySelector('#canvas');
+        const c = canvas.getContext('2d');
+
+
+        canvas.height = game.world.height;
+        canvas.width = game.world.width;
+        // game.world.player.x = canvas.width/2;
+        // game.world.player.y = canvas.height;
+
         // calling all other scripts and objects created
         function render(){
-            display.fill(game.world.backgroundColor);
-            display.drawRect(game.world.player.x, game.world.player.y, game.world.player.width, game.world.player.height, game.world.player.color );
-            display.render();
-        }
-
-        const canvas = document.querySelector('#canvas');
-        let display = new Display(canvas);
-        let game = new Game();
-
-       
-
-        setInterval(render, 1000/30);
-
+            display.renderCanvas(game.world.backgroundColor);
+            display.renderPlayer(game.world.player.x, game.world.player.y, game.world.player.width, game.world.player.height, game.world.player.color);
+            // display.render();
+        }        
+        render();
     })
 
 
